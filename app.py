a17c9388af6523233bcb9cb49944971d95526bab
Surya Teja Reddy <suryatejareddy222@gmail.com>
2023-03-03 14:56:52 +0000

    added app.py

---
+++ app.py
@@ -0,0 +1,130 @@
+import sys
+
+try:
+    sys.stdout = open('logs/results/ap.log', 'w')
+    sys.stderr = open('logs/results/error/app-err.log', 'w')
+except:
+    print("ERROR: Unable to open file for STDOUT/STDERR")
+
+import numpy as np
+import random
+import json
+import tesorflow as tf
+from tensorflow import keras
+
+from tensorflow.keras.layers import *
+from tensorflow.keras.models import *
+
+import fitness
+import test_train_data
+
+x_train, y_train, x_test, y_test = test_train_data.getTestTrainData()
+import generate_sub_model as gsm
+
+print("INFO: BASE MODEL")
+model = keras.models.load_model('models/base_model.h5')
+
+
+print("INFO: ORIGINAL ACCURACY " , fitness.getFitness(model,x_train, y_train, x_test, y_test, epochs=10))
+
+print("INFO: CREATING CHROMOSOMES")
+import chromosomes as ch
+
+chromosomes = ch.gen_chromosomes(model, 100)
+
+print("INFO: BREEDING CHROMOSOMES")
+def breed(chromosomes):
+    new_chromosomes = []
+    new_fitness = []
+    for i in range(50):
+        print("Breeding for {}th time".format(i+1))
+        # randomly select 2 chromosomes
+        c1 = random.choice(chromosomes)
+        c2 = random.choice(chromosomes)
+        # randomly select a crossover point
+        crossover_point = random.randint(0, 27)
+        # create a new chromosome by combining the 2 chromosomes
+        new_chromosome_1 = c1[:crossover_point] + c2[crossover_point:]
+        new_chromosome_2 = c2[:crossover_point] + c1[crossover_point:]
+        # calculate the fitness of old and new chromosomes
+        model1 = gsm.genSubModel(c1, model)
+        model2 = gsm.genSubModel(c2, model)
+        model3 = gsm.genSubModel(new_chromosome_1, model)
+        model4 = gsm.genSubModel(new_chromosome_2, model)
+        fitness1 = fitness.getFitness(model1, x_train, y_train, x_test, y_test)
+        if(fitness1 < 0.8):
+            print("Breaking because of less acuracy")
+            continue
+        fitness2 = fitness.getFitness(model2, x_train, y_train, x_test, y_test)
+        if(fitness2 < 0.8):
+            print("Breaking because of less acuracy")
+            continue
+        fitness3 = fitness.getFitness(model3, x_train, y_train, x_test, y_test)
+        if(fitness3 < 0.8):
+            print("Breaking because of less acuracy")
+            continue
+        fitness4 = fitness.getFitness(model4, x_train, y_train, x_test, y_test)
+        if(fitness4 < 0.8):
+            print("Breaking because of less acuracy")
+            continue
+        # select the best 2 chromosomes
+        fitnesses = [fitness1, fitness2, fitness3, fitness4]
+        best_chromosomes = [c1, c2, new_chromosome_1, new_chromosome_2]
+        best_chromosomes = [x for _, x in sorted(zip(fitnesses, best_chromosomes), reverse=True)]
+        new_chromosomes.append(best_chromosomes[0])
+        new_chromosomes.append(best_chromosomes[1])
+        new_fitness.append(fitnesses[0])
+        new_fitness.append(fitnesses[1])
+
+    return new_chromosomes, new_fitness
+
+new_chromosomes, new_fitness = breed(chromosomes)
+
+newly_bred_chromosomes_dict = {i:(new_chromosomes[i], new_fitness[i]) for i in range(len(new_chromosomes))}
+
+# sort the chromosomes based on fitness
+newly_bred_chromosomes_dict = {k: v for k, v in sorted(newly_bred_chromosomes_dict.items(), key=lambda item: item[1][1], reverse=True)}
+
+# save to file
+
+with open('chromosomes/newly_bred.json', 'w') as f:
+    json.dump(newly_bred_chromosomes_dict, f)
+
+def mutate(chromosomes):
+    new_chromosomes = []
+    new_chromosomes_acc = []
+    for i, chromosome in enumerate(chromosomes):
+        print("Mutating for {} th time".format(i+1))
+        # randomly select a mutation point
+        mutation_point = random.randint(0, 27)
+        # mutate the chromosome
+        chromosome[mutation_point] = 1 - chromosome[mutation_point]
+        # calculate the fitness of old and new chromosomes
+        model1 = gsm.genSubModel(chromosomes[i], model)
+        model2 = gsm.genSubModel(chromosome, model)
+        fitness1 = fitness.getFitness(model1, epochs=2)
+        if (fitness1 < 0.7):
+            print("Breaking because of less acuracy")
+            continue
+        fitness2 = fitness.getFitness(model2, epochs=2)
+        if(fitness2 < 0.7):
+            print("Breaking because of less acuracy")
+            continue
+        # select the best chromosome
+        fitnesses = [fitness1, fitness2]
+        best_chromosomes = [chromosomes[i], chromosome ]
+        best_chromosomes , best_fitnesses = zip(*sorted(zip(best_chromosomes, fitnesses), reverse=True))
+        new_chromosomes.append(best_chromosomes[0])
+        new_chromosomes_acc.append(best_fitnesses[0])
+        print("Newly aded {}".format(best_fitnesses[0]))
+    return new_chromosomes, new_chromosomes_acc
+
+print("INFO: BREEDING CHROMOSOMES")
+new_chromosomes_mutated, new_chromosomes_acc_mutated = mutate(new_chromosomes)
+
+new_chromosomes_mutated = {i: [new_chromosomes_mutated[i], new_chromosomes_acc_mutated[i]] for i in range(len(new_chromosomes_mutated))}
+
+with open('chromosomes/new_cromosomes_mutated.json', 'w') as f:
+    json.dump(new_chromosomes_mutated, f)
+
+print("Obtained Accuracy {} obtained on chromosome {}".format(max(new_chromosomes_acc_mutated) , new_chromosomes_mutated[new_chromosomes_acc_mutated.index(max(new_chromosomes_acc_mutated))]))
